806a49ddd94bf19c5b05d87279050629f95a2e0c
Renan Silva <renansilvaytB@gmail.com>
2024-03-13 17:53:34 -0300

    fix max day for get next day weather

--- src/utils/getNextDays.ts
+++ src/utils/getNextDays.ts
@@ -1,10 +1,11 @@
 import dayjs from "dayjs";
+const maxDays = 5;
 
 export function getNextDays() {
   const days: string[] = [];
   const today = new Date();
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < maxDays; i++) {
     const date = new Date();
     date.setDate(today.getDate() + (i + 1));
     days.push(dayjs(date).format('DD/MM'));
