63b20955c1441119fe1ec7d8ab84b41531e170ce
Renan Silva <renansilvaytB@gmail.com>
2024-03-13 18:07:21 -0300

    feat city props and fix to ts

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,12 +1,12 @@
 import './styles.css';
-
 import { useNavigate } from 'react-router-dom';
 import { SelectCity } from '../../components/SelectCity';
+import { CityProps } from '../../services/getCityByNameService';
 
 export function Search() {
   const navigate = useNavigate();
 
-  function handleSelected(city) {
+  function handleSelected(city: CityProps) {
     localStorage.setItem('@typewheather:city', JSON.stringify(city));
     navigate('/dashboard');
   }
@@ -14,11 +14,9 @@ export function Search() {
   return (
     <div className="home">
       <img src="logo_large.svg" alt="Logo" />
-
       <main>
         <h1>Boas vindas ao <strong>TypeWeather</strong></h1>
         <span>Escolha um local para ver a previsão do tempo</span>
-
         <SelectCity onSelect={handleSelected} />
       </main>
     </div>
